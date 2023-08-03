f70458713376b507eb311c10474aea52248fc736
angwoo0503 <angwoo0503@gmail.com>
2023-08-03 18:09:03 +0900

    add: Textfield function

--- TextFieldProject/ViewController.swift
+++ TextFieldProject/ViewController.swift
@@ -31,30 +31,49 @@ class ViewController: UIViewController, UITextFieldDelegate {
     
     // 텍스트필드의 입력을 시작할 때 호출 (시작할지 말지의 여부 허락하는 것)
     func textFieldShouldBeginEditing(_ textField: UITextField) -> Bool {
+        print(#function)
         return true
-        
     }
     
     
     // 시점 -
     func textFieldDidBeginEditing(_ textField: UITextField) {
+        print(#function)
         print("유저가 텍스트필드의 입력을 시작했다.")
     }
     
     func textFieldShouldClear(_ textField: UITextField) -> Bool {
+        print(#function)
         return true
     }
     
-    
+    // 텍스필드 글자 내용이 (한글자 한글자) 입력되거나 지워질 때 호출이 되고 (허락)
     func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
-        <#code#>
+        print(#function)
+        print(string)
+        return true
     }
     
+    // 텍스트필드의 엔터키가 눌러지면 다음 동작을 허락할 것인지 말 것인지
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        print(#function)
+        return true
+    }
+
+    // 텍스트필드의 입력이 끝날 때 호출 (끝날지 말지를 허락)
+    func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
+        print(#function)
+        return true
+    }
     
-    
+    // 텍스트필드의 입력이 끝났을때 호출 (시점)
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        print(#function)
+        print("유저가 텍스트필드의 입력을 끝냈다.")
+    }
     
     @IBAction func doneButtonTapped(_ sender: UIButton) {
-        
+
     }
     
 }
