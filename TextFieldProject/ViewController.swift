b1c49a2a1af7d9a9158deaa12bc61b93536f561b
angwoo0503 <angwoo0503@gmail.com>
2023-08-03 11:09:31 +0900

    add : Auto layout setting

---
+++ TextFieldProject/ViewController.swift
@@ -0,0 +1,61 @@
+//
+//  ViewController.swift
+//  TextFieldProject
+//
+//  Created by t2023-m0061 on 2023/08/02.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITextFieldDelegate {
+
+    @IBOutlet weak var textField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        textField.delegate = self
+        
+        setup()
+    }
+
+    func setup() {
+        view.backgroundColor = UIColor.gray
+        
+        textField.keyboardType = UIKeyboardType.emailAddress
+        textField.placeholder = "이메일 입력"
+        textField.borderStyle = .roundedRect
+        textField.clearButtonMode = .always
+        textField.returnKeyType = .next
+    }
+    
+    // 텍스트필드의 입력을 시작할 때 호출 (시작할지 말지의 여부 허락하는 것)
+    func textFieldShouldBeginEditing(_ textField: UITextField) -> Bool {
+        return true
+        
+    }
+    
+    
+    // 시점 -
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        print("유저가 텍스트필드의 입력을 시작했다.")
+    }
+    
+    func textFieldShouldClear(_ textField: UITextField) -> Bool {
+        return true
+    }
+    
+    
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        <#code#>
+    }
+    
+    
+    
+    
+    @IBAction func doneButtonTapped(_ sender: UIButton) {
+        
+    }
+    
+}
+
