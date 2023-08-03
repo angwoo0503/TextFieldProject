00469d0599fabda70c8e4b5b92f0f9bc2ed17e66
angwoo0503 <angwoo0503@gmail.com>
2023-08-03 23:08:17 +0900

    add: Textfield empty function

--- TextFieldProject/ViewController.swift
+++ TextFieldProject/ViewController.swift
@@ -49,15 +49,29 @@ class ViewController: UIViewController, UITextFieldDelegate {
     
     // 텍스필드 글자 내용이 (한글자 한글자) 입력되거나 지워질 때 호출이 되고 (허락)
     func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
-        print(#function)
-        print(string)
-        return true
+//        print(#function)
+//        print(string)
+        // 입력되고 있는 글자가 "숫자"인 경우 입력을 허용하지 않는 논리
+        if Int(string) != nil {
+            return false
+        } else {
+            // 10 글자 이상 입력되는 것을 막는 코드
+            guard let text = textField.text else { return true }
+            let newLength = text.count + string.count - range.length
+            return newLength <= 10
+        }
     }
     
     // 텍스트필드의 엔터키가 눌러지면 다음 동작을 허락할 것인지 말 것인지
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
         print(#function)
-        return true
+
+        if textField.text == "" {
+            textField.placeholder = "Type Something!"
+            return false
+        } else {
+            return true
+        }
     }
 
     // 텍스트필드의 입력이 끝날 때 호출 (끝날지 말지를 허락)
@@ -70,6 +84,7 @@ class ViewController: UIViewController, UITextFieldDelegate {
     func textFieldDidEndEditing(_ textField: UITextField) {
         print(#function)
         print("유저가 텍스트필드의 입력을 끝냈다.")
+        textField.text = ""
     }
     
     @IBAction func doneButtonTapped(_ sender: UIButton) {
